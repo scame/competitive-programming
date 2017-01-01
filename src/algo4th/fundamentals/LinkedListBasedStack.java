091968eb1fa412bb346257574876a83b2b94ccf3
Slava Petrochenko <vs.petrochenko@gmail.com>
2017-01-01 20:04:47 +0200

    added copy/find/peek methods

--- src/algo4th/fundamentals/LinkedListBasedStack.java
+++ src/algo4th/fundamentals/LinkedListBasedStack.java
@@ -26,6 +26,22 @@ public class LinkedListBasedStack<T>  implements Iterable<T> {
         return item;
     }
 
+    public T peek() {
+        if (isEmpty()) {
+            throw  new EmptyStackException();
+        }
+        return first.getItem();
+    }
+
+    public boolean find(T target) {
+        for (T elem : this) {
+            if (elem.equals(target)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public int size() {
         return nodeNumbers;
     }
@@ -37,5 +53,18 @@ public class LinkedListBasedStack<T>  implements Iterable<T> {
     @Override
     public Iterator<T> iterator() {
         return new BasicStructsIterator<>(first);
+    }
+
+    public static <E> LinkedListBasedStack<E> copy(LinkedListBasedStack<E> source) {
+        LinkedListBasedStack<E> reverseStack = new LinkedListBasedStack<>();
+        LinkedListBasedStack<E> copy = new LinkedListBasedStack<>();
+
+        for (E element : source) {
+            reverseStack.push(element);
+        }
+        for (E element : reverseStack) {
+            copy.push(element);
+        }
+        return copy;
     }
 }
